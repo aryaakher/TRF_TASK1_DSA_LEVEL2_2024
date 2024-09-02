59dd3f3d5706e9e8bbfe43b12ac6d550910c70be
aryaakher <aryaa.kher23@vit.edu>
2024-09-02 23:08:21 +0530

    Task 2 Commit 1

---
+++ AryaaKher_ZeroSumSubarrays_Task2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void findZeroSumSubarrays(int arr[], int n) {
+    int found = 0;
+
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = i; j < n; j++) {
+            sum += arr[j];
+            if (sum == 0) {  // Corrected the condition here
+                found = 1;
+                for (int k = i; k <= j; k++) {
+                    printf("%d ", arr[k]);
+                }
+                printf("\n");
+            }
+        }
+    }
+
+    if (!found) {
+        printf("[]\n");
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];  // VLA: be cautious with non-standard compilers
+
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    findZeroSumSubarrays(arr, n);
+
+    return 0;
+}
